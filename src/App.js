7f31d65879dd2ef670cfa6aff2ec7d08ea21cbcc
Anthony Quist <aquist@rocketmail.com>
2022-07-24 14:30:09 -0400

    initial commit for tempearture control app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+ 
 function App() {
+  const [tempVal, setempVal] = useState(50);
+  const [tempColor, settempColor] = useState("neutral")
+
+  const  incr_temp = () => {
+      const newTemp = tempVal + 1
+      setempVal(newTemp);
+
+     if(newTemp > 50){settempColor("hot");}
+     else{settempColor("neutral");}
+    }
+
+  const desc_temp = () => {
+    const newTemp = tempVal - 1
+    setempVal(newTemp);
+
+    if(newTemp < 50){settempColor("cold");}
+    else{settempColor("neutral");}
+  }  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="temp_ctr">
+      <div className="temp_disp" >
+        <div className={`temp_count ${tempColor}`}>{tempVal}°F</div>
+      </div>
+
+      <div className="temp_buttons">
+        <button className="decr" onClick= {() => desc_temp()}>-</button>
+        <button className="incr" onClick= {() => incr_temp()}>+</button>
+      </div>
     </div>
   );
 }
